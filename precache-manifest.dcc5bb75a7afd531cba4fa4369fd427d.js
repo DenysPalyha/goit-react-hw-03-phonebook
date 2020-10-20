e1b02d7d5ff056f9e91bc3c6e75b1a8f70ef8fd3
unknown <you@example.com>
2020-10-20 18:40:08 +0300

    Updates

---
+++ precache-manifest.dcc5bb75a7afd531cba4fa4369fd427d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2029a660ba559087f08910dd9663dc8a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "3b3d780508a63ca66265",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.26f8390d.chunk.css"
+  },
+  {
+    "revision": "048a37132fb67da1ad8b",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8a8aa1d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.8a8aa1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3d780508a63ca66265",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.b2d9cad9.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
